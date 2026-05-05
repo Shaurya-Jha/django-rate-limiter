3f633db5e365b7c1fc79410fcfa47fca905508a3
Shaurya-Jha <shauryajha1602@gmail.com>
2026-05-06 00:12:41 +0530

    user and task model setup

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    title = models.TextField(required=True, null=False)
+    description = models.TextField(max_length=200)
+    category = models.TextField(max_length=100)
+    created_by = models.ForeignKey("User", on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey("User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        if (self.created_by == self.assigned_to):
+            return f"{self.created_by} assigned task {self.title} to self"
+        return f"{self.created_by} assigned task {self.title} to {self.assigned_to}"
